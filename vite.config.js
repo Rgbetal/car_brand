4407e9123a272a1c0f372985bda8764e9d4f15ba
Rgbetal <rachidoukabore440@gmail.com>
2025-05-22 17:58:11 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     build: {
-        outDir: "dist",
+        outDir: "public/build",
     },
     plugins: [
         laravel({
